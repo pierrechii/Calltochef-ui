8c37c18ac4c0c93f12b383ee6323a92fd6116ed7
pierrechii <chirosselpierre@gmail.com>
2025-10-17 15:49:03 +0200

    fix: suppression des badges de confiance faux et correction erreur ChefsSection

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       <Header />
       <main>
         <HeroSection />
-        <TrustBadges />
         <AdvantagesSection />
         <ProcessSection />
         <TestimonialsSection />
